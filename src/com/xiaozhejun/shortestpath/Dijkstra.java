9e3b342352f6fabceec5d11314da7c409078b013
LucasYang7 <731180001@qq.com>
2016-10-28 17:10:26 +0000

    add Dijkstra algorithm

---
+++ src/com/xiaozhejun/shortestpath/Dijkstra.java
@@ -0,0 +1,49 @@
+package com.xiaozhejun.shortestpath;
+
+import com.xiaozhejun.storage.AdjacencyMatrix;
+
+public class Dijkstra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dijkstra dijkstra = new Dijkstra();
+		dijkstra.dijkstra(0, 2, AdjacencyMatrix.graph2);
+	}
+	
+	public void dijkstra(int start,int destination,int[][] graphAdj){
+		int numberOfNodes = graphAdj.length;            // 图中的结点个数
+		int[] dist = new int[numberOfNodes];            // 图中各个顶点到起始顶点的距离
+		boolean[] visited = new boolean[numberOfNodes]; // 判断顶点i是否已经加入到了已求出距离的顶点集合       
+		for(int i = 0;i < numberOfNodes;i++){
+			dist[i] = Integer.MAX_VALUE;
+			visited[i] = false;
+		}
+		dist[start] = 0;           // start顶点到自身的距离为０
+		for(int i = 0;i < numberOfNodes;i++){
+			int curVisitNode = -1;
+			for(int j = 0;j < numberOfNodes;j++){
+				if(visited[j] == true)
+					continue;
+				if(curVisitNode == -1 || dist[j] < dist[curVisitNode]){// 在未求出最短距离的顶点集合中找出一个距离start顶点最近的顶点
+					curVisitNode = j;
+				}
+			}
+			visited[curVisitNode] = true;
+			// 将curVisitNode顶点加入到已求出最短路径的顶点集合后，更新图中其余各个顶点到start顶点的最短距离
+			for(int j = 0;j < numberOfNodes;j++){
+				// graphAdj[curVisitNode][j] != MAX保证curVisitNode与j顶点之间有一条边
+				// 如果graphAdj[curVisitNode][j] == MAX，那么 dist[curVisitNode] + graphAdj[curVisitNode][j] < 0
+				int minDistance = dist[curVisitNode] + graphAdj[curVisitNode][j];
+				if(minDistance >= 0 && minDistance < dist[j]){
+					dist[j] = minDistance;
+				}
+			}
+		}
+		//System.out.println("start node to destination node's distance is " + dist[destination]);
+		for(int i = 0;i < numberOfNodes;i++){
+			System.out.println("start node " + start + " to destination " + i + " node's distance is " + dist[i]);
+		}
+		
+	}
+
+}
--- src/com/xiaozhejun/storage/AdjacencyMatrix.java
+++ src/com/xiaozhejun/storage/AdjacencyMatrix.java
@@ -24,4 +24,14 @@ public class AdjacencyMatrix {
             {6, 8, 0, 0, 9},
             {0, 5, 7, 9, 0},
            };
+	
+	public static final int MAX	= Integer.MAX_VALUE;
+	
+	public static int[][] graph2 = {
+			{MAX, 2, MAX, 6, MAX},
+            {2, MAX, 3, 8, 5},
+            {MAX, 3, MAX, MAX, 7},
+            {6, 8, MAX, MAX, 9},
+            {MAX, 5, 7, 9, MAX},
+           };
 }
